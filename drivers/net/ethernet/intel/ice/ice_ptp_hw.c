2a837afb2c753c1bc6c900a9acb6a07ac9af0054
Jacob Keller <jacob.e.keller@intel.com>
2026-09-25 16:19:30 -0700

    ice: E825: perform a soft reset when starting the PHY timer
    To ensure that the E825 PHY timer begins in a clean state, initiate a PHY
    soft reset prior to programming the PHY. This ensures that we clear any
    outstanding Tx timestamp memory, and ensures that the PHY internal state
    including its timestamp counter has been reset.
    
    Note that despite the name, this PHY soft reset only impacts the timer
    block of the PHY, and not its regular Tx/Rx functionality. Additionally,
    many configuration registers such as the PHY_REG_TS_INT_CONFIG and
    PHY_REG_TX_OFFSET_READY are not reset.
    
    Fixes: 7cab44f1c35f ("ice: Introduce ETH56G PHY model for E825C products")
    Reviewed-by: Maciek Machnikowski <maciej.machnikowski@intel.com>
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ptp_hw.c
+++ drivers/net/ethernet/intel/ice/ice_ptp_hw.c
@@ -2128,9 +2128,11 @@ int ice_stop_phy_timer_eth56g(struct ice_hw *hw, u8 port)
  * @hw: pointer to the HW struct
  * @port: the PHY port to start
  *
- * Start the clock of a PHY port. This must be done as part of the flow to
- * re-calibrate Tx and Rx timestamping offsets whenever the clock time is
- * initialized or when link speed changes.
+ * Perform a PHY soft reset and then start the clock for the PHY port.
+ *
+ * This must be done as part of the flow to re-calibrate Tx and Rx
+ * timestamping offsets whenever the clock time is initialized or when link
+ * speed changes.
  *
  * Return:
  * * %0     - success
@@ -2147,6 +2149,10 @@ int ice_start_phy_timer_eth56g(struct ice_hw *hw, u8 port)
 	tmr_idx = ice_get_ptp_src_clock_index(hw);
 
 	err = ice_stop_phy_timer_eth56g(hw, port);
+	if (err)
+		return err;
+
+	err = ice_ptp_phy_soft_reset_eth56g(hw, port);
 	if (err)
 		return err;
 
